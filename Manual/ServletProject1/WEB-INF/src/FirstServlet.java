b87a6d48a68ea6f85b63f1a4d3b03143113cbdf4
konny <istjprogrammer@gmail.com>
2023-09-13 15:12:56 +0900

    첫번째 커밋

---
+++ Manual/ServletProject1/WEB-INF/src/FirstServlet.java
@@ -0,0 +1,7 @@
+import javax.servlet.http.*;
+
+public class FirstServlet extends HttpServlet {
+	protected void 	doGet(HttpServletRequest req, HttpServletResponse resp){
+		System.out.println("¼­ºí¸´ ½ÇÇà");
+	}
+}
