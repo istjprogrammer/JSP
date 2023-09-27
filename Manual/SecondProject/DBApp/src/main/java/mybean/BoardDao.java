9a8ed5f4390f8476eabad84dacbef8e86d3c5b21
konny <istjprogrammer@gmail.com>
2023-09-27 16:25:10 +0900

    jsp bean 파일 수정

--- Manual/SecondProject/DBApp/src/main/java/mybean/BoardDao.java
+++ Manual/SecondProject/DBApp/src/main/java/mybean/BoardDao.java
@@ -33,13 +33,13 @@ public class BoardDao {
 		if(con != null)	try {con.close();} catch (SQLException e) {	e.printStackTrace();}
 	}
 	
-	// List.jsp
+	// List.jsp, Update.jsp
 	public List getBoardList() {
 		String sql = "select b_num, b_subject, b_name, b_regdate, b_count from tblboard";
 		Vector vector = new Vector();
 		
 		try {
-			con = ds.getConnection();//DB접속 코드
+			con = ds.getConnection(); 
 			stmt = con.prepareStatement(sql);
 			rs = stmt.executeQuery();
 			
@@ -63,29 +63,107 @@ public class BoardDao {
 		return vector;
 	}
 	
-	//PostProc.jsp(글 쓰기 기능을 Bean으로 옮길 예정)
+	// PostProc.jsp
 	public void setBoard(Board board) {
 		String sql = "insert into tblboard(b_num," +
 				"b_name, b_email, b_homepage, b_subject, b_content, " +
 				"b_pass, b_count, b_ip, b_regdate, pos, depth) " +
 				"values(seq_b_num.nextVal, ?,?,?,?,?,?, 0, ?, sysdate, 0, 0)";
-	try {
-		stmt = con.prepareStatement(sql);
-		//Bean에서 뽑아내는 기능
-		stmt.setString(1, board.getB_name());
-		stmt.setString(2, board.getB_email());
-		stmt.setString(3, board.getB_homepage());
-		stmt.setString(4, board.getB_subject());
-		stmt.setString(5, board.getB_content());
-		stmt.setString(6, board.getB_pass());
-		stmt.setString(7, board.getB_ip());
-		stmt.executeUpdate();	
+		try {
+			con = ds.getConnection(); 
+			stmt = con.prepareStatement(sql);
+			stmt.setString(1, board.getB_name());
+			stmt.setString(2, board.getB_email());
+			stmt.setString(3, board.getB_homepage());
+			stmt.setString(4, board.getB_subject());
+			stmt.setString(5, board.getB_content());
+			stmt.setString(6, board.getB_pass());
+			stmt.setString(7, board.getB_ip());
+			stmt.executeUpdate();
+		}
+		catch(Exception e) {
+			System.out.println("setBoard : " + e);
+		}
+		finally {
+			freeConnection();
+		}
 	}
-	catch(Exception e) {
-		System.out.println("setBoard : " + e);
+	
+	// Read.jsp, Update.jsp
+	public Board getBoard(int b_num){
+		String sql;
+		Board result = new Board();
+		
+		try{
+			con = ds.getConnection();
+			
+			sql = "select * from tblboard where b_num=?";
+			stmt = con.prepareStatement(sql);
+			stmt.setInt(1, b_num);
+			rs = stmt.executeQuery();
+			
+			if(rs.next()){
+				result.setB_content(rs.getString("b_content"));
+				result.setB_count(rs.getInt("b_count"));
+				result.setB_email(rs.getString("b_email"));
+				result.setB_homepage(rs.getString("b_homepage"));
+				result.setB_ip(rs.getString("b_ip"));
+				result.setB_name(rs.getString("b_name"));
+				result.setB_num(rs.getInt("b_num"));
+				result.setB_pass(rs.getString("b_pass"));
+				result.setB_regdate(rs.getString("b_regdate"));
+				result.setB_subject(rs.getString("b_subject"));
+			}
+		}
+		catch(Exception err){
+			System.out.println("getBoard() : " + err);
+		}
+		finally{
+			freeConnection();
+		}
+		
+		return result;
 	}
-	finally {
-		freeConnection();
+	
+	// UpdateProc.jsp
+	public void updateBoard(Board dto){
+		String sql = "update tblboard set b_name=?, b_email=?, " +
+			"b_subject=?, b_content=? where b_num=?";
+		
+		try{
+			con = ds.getConnection();
+			stmt = con.prepareStatement(sql);
+			stmt.setString(1, dto.getB_name());
+			stmt.setString(2, dto.getB_email());
+			stmt.setString(3, dto.getB_subject());
+			stmt.setString(4, dto.getB_content());
+			stmt.setInt(5, dto.getB_num());
+			
+			stmt.executeUpdate();
+		}
+		catch(Exception err){
+			System.out.println("updateBoard() : " + err);
+		}
+		finally{ freeConnection(); }
 	}
+	
+	// Delete.jsp
+	public void deleteBoard(int b_num){
+		String sql = "delete from tblboard where b_num=?";
+			
+		try{
+			con = ds.getConnection();
+			stmt = con.prepareStatement(sql);
+			stmt.setInt(1, b_num);
+			stmt.executeUpdate();
+		}
+		catch(Exception err){
+			System.out.println("deleteBoard() : " + err);
+		}
+		finally{ freeConnection(); }
 	}
 }
+
+
+
+
